aa139b4bd5d399391b7101884383ace6f48f9181
TeerthKr <tkunhiraman@conga.com>
2023-02-12 18:51:20 +0530

    End of section 3

---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using System.Reflection.Metadata.Ecma335;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsnc()
+        {
+            return await nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
