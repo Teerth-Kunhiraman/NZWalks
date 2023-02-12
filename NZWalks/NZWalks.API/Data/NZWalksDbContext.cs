93ec009fb310dcd96ccee9c176fce561b4cc1c3f
TeerthKr <tkunhiraman@conga.com>
2023-02-12 15:58:40 +0530

    End of session 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions {get; set; }//It is used to tell entity framweork that please create region table if it exist in database
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+    }
+}
